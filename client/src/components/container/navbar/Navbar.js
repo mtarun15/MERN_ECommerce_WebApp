2b7d8659f0fbca722098a64ce07784abfaf28394
RaghunathVadlakonda <sagarkharabe97@gmail.com>
2019-02-21 18:14:44 +0530

    final for long time

--- client/src/components/container/navbar/Navbar.js
+++ client/src/components/container/navbar/Navbar.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
+
 // const { user } = this.props;
 
 export class Navbar extends React.Component {
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import store from "./redux/store";
-
 ReactDOM.render(
   <Provider store={store}>
     <Router>
